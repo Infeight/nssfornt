a2e3a1d58d196ec25c34f7c9d246ebf51f108fc9
Infeight <167733649+Infeight@users.noreply.github.com>
2024-08-10 19:44:36 +0530

    Update events.js

--- events.js
+++ events.js
@@ -2,7 +2,7 @@
 
 
 const events = async()=>{
-  const data = await fetch('https://nss-server-zeta.vercel.app/events');
+  const data = await fetch('https://nss-server-zeta.vercel.app/events',{headers:{'Access-Control-Allow-Origin': 'https://nssiiitdmkurnool.netlify.app/events'}});
   const pdfdata = await fetch('https://nss-server-zeta.vercel.app/eventpdf');
   const allevents = await data.json()
   const allpdfs = await pdfdata.json()
